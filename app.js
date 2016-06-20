c0a6ee73bdc080618a25dc26b467f974fedc048b
Alexander Penkin <a.penkin@smartcodes.ru>
2016-06-20 16:16:42 +0300

    NodeJS basic server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + 'public'));
+
+app.listen('8888', function(){
+  console.log("worked on 8888 port")
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
